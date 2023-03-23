0bb82a89337649dd17c2afae5556f4ac197b1c1e
som <rudwns708@naver.com>
2023-03-23 18:53:03 +0900

    fix head

--- v1.py
+++ v1.py
@@ -62,22 +62,32 @@ def estimate_loss():
     return out
 
 class Head(nn.Module):
+    """ one head of self-attention """
+
     def __init__(self, head_size):
         super().__init__()
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
-        self.register_buffer('tril', torch.tril(torch.ones(block_size,block_size)))
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
         self.dropout = nn.Dropout(dropout)
-        self.head_size = head_size
+
     def forward(self, x):
-        B, T, C = x.shape
-        k, q = self.key(x), self.query(x).transpose(-2,-1)
-        wei = (k @ q) * self.head_size**0.5
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
-        wei = F.softmax(wei, dim=-1)
+        # input of size (batch, time-step, channels)
+        # output of size (batch, time-step, head size)
+        B,T,C = x.shape
+        k = self.key(x)   # (B,T,hs)
+        q = self.query(x) # (B,T,hs)
+        # compute attention scores ("affinities")
+        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
         wei = self.dropout(wei)
-        return wei @ self.value(x) 
+        # perform the weighted aggregation of the values
+        v = self.value(x) # (B,T,hs)
+        out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
+        return out
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size) -> None:
