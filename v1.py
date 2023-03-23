bbf1ac29afffe5566c894bc724a85e866f6b6344
pkj <rudwns708.14564@gmail.com>
2023-03-23 13:39:32 +0900

    train basic

--- v1.py
+++ v1.py
@@ -46,3 +46,57 @@ def get_batch(split):
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
     x, y = x.to(device), y.to(device)
     return x, y
+
+
+xb, yb = get_batch('train')
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class BigramLanguageModel(nn.Module):
+  def __init__(self, vocab_size):
+    super().__init__()
+    self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+    
+  def forward(self, idx, targets=None):
+    logits = self.token_embedding_table(idx)
+
+    if targets == None:
+      loss = None
+    else:
+      B, T, C = logits.shape
+      logits = logits.view(B*T, C)
+      targets = targets.view(-1)
+      loss = F.cross_entropy(logits, targets)
+    return logits, loss
+  
+  def generate(self, idx, max_new_tokens):
+    for i in range(max_new_tokens):
+      logits, loss = self(idx)
+      logits = logits[:, -1, :] # (B, 1, C)
+      probs = F.softmax(logits, dim=-1) # get max
+      idx_next = torch.multinomial(probs, num_samples=1) # (B, 1) get max index
+      idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+    return idx
+
+m = BigramLanguageModel(vocab_size)
+logits, loss = m(xb, yb)
+
+# create a PyTorch optimizer
+optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+
+batch_size = 32
+for steps in range(10000): # increase number of steps for good results... 
+    
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = m(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+print(loss.item())
+print(decode(m.generate(idx = torch.ones((1,1), dtype=torch.long), max_new_tokens=500)[0].tolist()))
