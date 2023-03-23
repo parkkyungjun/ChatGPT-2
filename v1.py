72ac23fe93e8ee8c00aaa98220fc1951ff99b9b8
som <rudwns708@naver.com>
2023-03-23 20:36:17 +0900

    add generate @torch.no_grad()

--- v1.py
+++ v1.py
@@ -213,7 +213,8 @@ class GPTLanguageModel(nn.Module):
             loss = F.cross_entropy(logits, targets)
 
         return logits, loss
-
+        
+    @torch.no_grad()
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
