496f83379b50928796cf38767774b555944989ac
pkj <rudwns708.14564@gmail.com>
2023-03-23 13:58:01 +0900

    make multihead

--- v1.py
+++ v1.py
@@ -67,33 +67,82 @@ class Head(nn.Module):
         wei = self.dropout(wei)
         return wei @ self.value(x) 
 
-class BigramLanguageModel(nn.Module):
-  def __init__(self, vocab_size):
-    super().__init__()
-    self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
-    
-  def forward(self, idx, targets=None):
-    logits = self.token_embedding_table(idx)
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size) -> None:
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
-    if targets == None:
-      loss = None
-    else:
-      B, T, C = logits.shape
-      logits = logits.view(B*T, C)
-      targets = targets.view(-1)
-      loss = F.cross_entropy(logits, targets)
-    return logits, loss
-  
-  def generate(self, idx, max_new_tokens):
-    for i in range(max_new_tokens):
-      logits, loss = self(idx)
-      logits = logits[:, -1, :] # (B, 1, C)
-      probs = F.softmax(logits, dim=-1) # get max
-      idx_next = torch.multinomial(probs, num_samples=1) # (B, 1) get max index
-      idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
-    return idx
-    
-m = BigramLanguageModel(vocab_size)
+    def forward(self, x):
+        out = torch.cat([head(x) for head in self.heads], dim=-1)
+        return self.dropout(self.proj(out))
+        
+class Block(nn.Module):
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = Mu
+class GPTLanguageModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+
+        # better init, not covered in the original GPT video, but important, will cover in followup video
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        x = tok_emb + pos_emb # (B,T,C)
+        x = self.blocks(x) # (B,T,C)
+        x = self.ln_f(x) # (B,T,C)
+        logits = self.lm_head(x) # (B,T,vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+
+m = GPTLanguageModel(vocab_size)
 logits, loss = m(xb, yb)
 
 # create a PyTorch optimizer
